9cc2dd0dfdb66848cf85752fa857d60001111d68
KassieZuritaG <57915876+KassieZuritaG@users.noreply.github.com>
2020-06-03 12:41:18 -0500

    PryEjercicio1V2
    Creación de Proyecto

---
+++ src/main/java/com/zurita/ejercicio/PryEjercicio1V2Application.java
@@ -0,0 +1,13 @@
+package com.zurita.ejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PryEjercicio1V2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PryEjercicio1V2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zurita/ejercicio/PryEjercicio1V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zurita.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PryEjercicio1V2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
